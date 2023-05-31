a58cee71fdaa66a5851183cb0ff0bd3d0253bba9
Vít Smolík <vit.smolik2@gmail.com>
2023-05-31 20:04:09 +0000

    rejected count

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<body>
+  <h1><b>How many times have I been rejected from Github Education Student Program</b></h1>
+  <div class="big-number">6 times</div>
+  <p>and counting...</p>
+</body>
+
+i
+<style>
+   div {
+      background-color: #f5f5f5;
+      text-align: center;
+    }
+		
+		h1 {
+  		font-size: 24px;
+  		font-weight: bold;
+  		margin-top: 100px;
+		text-align: center;
+		}
+
+		.big-number {
+  		font-size: 72px;
+  		font-weight: bold;
+  		margin-top: 50px;
+		background: inherit;
+		}
+
+		p {
+  		font-size: 14px;
+  		margin-bottom: 50px;
+		text-align: center;
+		}
+</style>
+
+
+
